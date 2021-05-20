cfdba66b1f4253c92c94b4d0725577de77301456
lmarqueo <lmo.marques.oliveira@gmail.com>
2021-05-20 14:26:46 -0300

    Criando instancia do RestTemplate capaz de se integrar com o Eureka

--- store/src/main/java/br/com/microservice/store/service/StoreService.java
+++ store/src/main/java/br/com/microservice/store/service/StoreService.java
@@ -2,6 +2,7 @@ package br.com.microservice.store.service;
 
 import br.com.microservice.store.controller.dto.StoreDTO;
 import br.com.microservice.store.service.dto.SupplierDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,16 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class StoreService {
 
+    private final RestTemplate client;
+
+    @Autowired
+    public StoreService(RestTemplate client) {
+        this.client = client;
+    }
+
     public void makePurchase(StoreDTO store) {
-        RestTemplate client = new RestTemplate();
         ResponseEntity<SupplierDTO> supplierDTOResponseEntity =
-                client.exchange("http://localhost:8081/info/" + store.getAddress().getState(),
+                client.exchange("http://supplier:8081/info/" + store.getAddress().getState(),
                     HttpMethod.GET, null, SupplierDTO.class);
         System.out.println(supplierDTOResponseEntity.getBody().getAddress());
     }
